13361ecdabc1fa3125941945af5857eda9073437
NadijaK <32327863+NadijaK@users.noreply.github.com>
2024-05-13 16:28:02 +0200

    Delete json_task.ps1

--- json_task.ps1
+++
@@ -1,6 +0,0 @@
-$json = Get-Content "data.json" | ConvertFrom-Json
-$additionalJson = Get-Content "additional_data.json" | ConvertFrom-Json
-# Add logic here to remove properties, add properties, and combine as needed.
-
-
-$combinedData | Export-Csv "output.csv" -NoTypeInformation
