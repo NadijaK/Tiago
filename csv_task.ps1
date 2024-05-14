76fbf9994363734176bd8919b525f6f4d36bfce9
NadijaK <32327863+NadijaK@users.noreply.github.com>
2024-05-14 13:28:09 +0200

    Delete csv_task.ps1

--- csv_task.ps1
+++
@@ -1,7 +0,0 @@
-$csv = Import-Csv "https://github.com/NadijaK/Tiago/blob/main/goodreads_cleaned.csv"
-$i = 1
-
-foreach ($entry in $csv) {
-Write-Host "The name $($entry.authorName) and the average rating $($entry.average_rating) is the $i the entry in the CSV."
-$i++
-}
