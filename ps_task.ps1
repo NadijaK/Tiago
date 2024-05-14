b3b43c1dc025edbd0e1f7381a421e344a9b094c0
NadijaK <32327863+NadijaK@users.noreply.github.com>
2024-05-14 09:23:17 +0200

    Add files via upload

---
+++ ps_task.ps1
@@ -0,0 +1,11 @@
+﻿$resources = Get-AzResource
+$resourceByType = New-Object PSObject
+$resourceByType | Add-Member -MemberType NoteProperty -Name "ResourceTypes" -Value @{} -Force
+foreach($resource in $resources){
+        if($resourceByType.ResourceTypes.keys -notcontains $resource.ResourceType){
+                $resourceByType.ResourceTypes.Add($resource.ResourceType, @())
+        }
+        $resourceByType.ResourceTypes[$resource.ResourceType] += $resource.Name
+}
+$resourceByTypeJson = $resourceByType | ConvertTo-Json -Depth 10
+New-Item -Name test.json -Path .\_PowershellPlayground\ -Value $resourceByTypeJson -Force
